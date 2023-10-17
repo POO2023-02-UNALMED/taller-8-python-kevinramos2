89092304fddbc141ec9f6af99799dcb8562782a3
kevinramos2 <keramosl@unal.edu.co>
2023-10-17 00:22:18 -0500

    futbolista pa

--- .github/futbolista.py
+++ .github/futbolista.py
@@ -16,3 +16,25 @@ class Futbolista(Persona,Deportista):
     @classmethod
     def setListaFutbolista(clc, listaFutbolistas):
         clc.listaFutbolistas = listaFutbolistas
+    @classmethod
+    def getListaFutbolista(clc):
+        return clc.listadoJugadores
+
+    def getGolesMarcados(self):
+        return self._golesMarcados
+    def setGolesMarcados(self,x):
+        self._golesMarcados = x
+
+    def getTarjetasRojas(self):
+        return self._tarjetasRojas
+    def setTarjetasRojas(self,x):
+        self._tarjetasRojas = x
+
+    def getPiernaHabil(self):
+        return self._piernaHabil
+    def setPiernaHabil(self,x):
+        self._piernaHabil = x
+
+    def __str__(self):
+        return f"Mi nombre es {self.getNombre()} soy profesional en el deporte {self.getDeporte()} Tengo {self.getEdad()} años de edad y llevo {self.getAñosPracticando()} años en el deporte"
+    
